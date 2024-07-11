28149588d101c367018fafdc1afdba566bce557e
Yura <yury.kirytkin@yandex.ru>
2024-07-11 23:15:14 +0300

    added name input (step 3)

---
+++ src/cli.js
@@ -0,0 +1,11 @@
+import readlineSync from "readline-sync";
+
+export const askForName = () => {
+  try {
+    const name = readlineSync.question("May I have your name? ");
+    console.log(`Hello, ${name}!`);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
