d04a1b32e0a8418f60c3b8de4a2068f386404606
Yura <yury.kirytkin@yandex.ru>
2024-07-15 20:36:48 +0300

    Update cli.js to return name as value

--- src/cli.js
+++ src/cli.js
@@ -4,8 +4,10 @@ export default () => {
   try {
     const name = readlineSync.question("May I have your name? ");
     console.log(`Hello, ${name}!`);
+    return name;
   } catch (err) {
     console.error(err);
     process.exit(1);
+    return '';
   }
 };
