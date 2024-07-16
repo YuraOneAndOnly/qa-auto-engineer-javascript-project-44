f9d488db8666b7b3b4c2719a237580e6e778761c
Yura <yury.kirytkin@yandex.ru>
2024-07-16 22:03:40 +0300

    Added commentary

--- src/brain-calc-game.js
+++ src/brain-calc-game.js
@@ -1,35 +1,38 @@
 import askQuestion from "./question-answer.js";
 
 export default (name) => {
-  let questionType = 0;
-  let questionFirstNumber = 0;
-  let questionSecondNumber = 0;
-  let expectedAnswer = 0;
-  let question = "";
-  let result = false;
-  let isBreaked = false;
+  let questionType = 0; // тип вопроса (0(+), 1(-), 2(*))
+  let questionFirstNumber = 0; // первое число в вопросе
+  let questionSecondNumber = 0; // второе число в вопросе
+  let expectedAnswer = 0; // ожидаемый ответ
+  let question = ""; // формируемая строка вопроса
+  let result = false; // результат ответа
+  let isBreaked = false; // условие завершения цикла
+  const maxNumber = 1000; // максимальное возможное значение чисел в вопросе
 
-  console.log("What is the result of the expression?")
+  console.log("What is the result of the expression?");
   for (let i = 1; i <= 3; i += 1) {
-    questionType = Math.floor(Math.random() * 3);
-    switch (questionType) {
+    questionType = Math.floor(Math.random() * 3); // генерация случайного числа от 0 до 2
+    switch (
+      questionType // 3 возможных исхода 0(+), 1(-), 2(*)
+    ) {
       case 1: {
-        questionFirstNumber = Math.floor(Math.random() * 1000);
-        questionSecondNumber = Math.floor(Math.random() * 1000);
+        questionFirstNumber = Math.floor(Math.random() * maxNumber);
+        questionSecondNumber = Math.floor(Math.random() * maxNumber);
         question = `${questionFirstNumber} - ${questionSecondNumber}`;
         expectedAnswer = questionFirstNumber - questionSecondNumber;
         break;
       }
       case 2: {
-        questionFirstNumber = Math.floor(Math.random() * 100);
-        questionSecondNumber = Math.floor(Math.random() * 100);
+        questionFirstNumber = Math.floor(Math.random() * Math.floor(maxNumber / 10));
+        questionSecondNumber = Math.floor(Math.random() * Math.floor(maxNumber / 10));
         question = `${questionFirstNumber} * ${questionSecondNumber}`;
         expectedAnswer = questionFirstNumber * questionSecondNumber;
         break;
       }
       default: {
-        questionFirstNumber = Math.floor(Math.random() * 1000);
-        questionSecondNumber = Math.floor(Math.random() * 1000);
+        questionFirstNumber = Math.floor(Math.random() * maxNumber);
+        questionSecondNumber = Math.floor(Math.random() * maxNumber);
         question = `${questionFirstNumber} + ${questionSecondNumber}`;
         expectedAnswer = questionFirstNumber + questionSecondNumber;
         break;
