dca1c5b6b442922508d55f73ba39ad4f841d702e
Yura <yury.kirytkin@yandex.ru>
2024-07-16 22:58:08 +0300

    eslint config test

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    extends: ["airbnb-base", "prettier"],
+    parserOptions: {
+      ecmaVersion: 15,
+      sourceType: "module",
+    },
+    rules: {
+      "no-console": "off",
+      "import/extensions": "off",
+    },
+  };
+  
