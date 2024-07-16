e24769f23fb3318d447f747fd05a903477fc1ae2
Yura <yury.kirytkin@yandex.ru>
2024-07-16 22:11:57 +0300

    Moved welcome message to ./src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from "readline-sync";
 
 export default () => {
+  console.log("Welcome to the Brain Games!");
   try {
     const name = readlineSync.question("May I have your name? ");
     console.log(`Hello, ${name}!`);
@@ -8,6 +9,6 @@ export default () => {
   } catch (err) {
     console.error(err);
     process.exit(1);
-    return '';
+    return "";
   }
 };
