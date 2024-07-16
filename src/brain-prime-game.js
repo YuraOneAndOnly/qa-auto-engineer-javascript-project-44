d25a6729cfa794259d87d44d485ea9fabc489b88
Yura <yury.kirytkin@yandex.ru>
2024-07-16 22:05:08 +0300

    Added commentary and remove magic numbers

--- src/brain-prime-game.js
+++ src/brain-prime-game.js
@@ -2,12 +2,14 @@ import askQuestion from "./question-answer.js";
 import primeNumberGenerator from "./prime-number-generator.js";
 
 export default (name) => {
-  let result = false;
-  let isBreaked = false;
-  let question = 1;
+  let result = false; // результат ответа
+  let isBreaked = false; // условие завершения цикла
+  let question = 1; // число для вопроса
+  const maxNumber = 100; // максимальное возможное значение чисел в вопросе
+
   console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
   for (let i = 1; i <= 3; i += 1) {
-    const chanceToBePrime = Math.floor(Math.random() * 100);
+    const chanceToBePrime = Math.floor(Math.random() * maxNumber);
     if (chanceToBePrime <= 40) {
       // шанс, что вопрос будет с простым числом устанавливается правым компонентом условия
       question = primeNumberGenerator(true);
