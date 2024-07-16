d5d0b932aaaa8d53cb3631089e8bc78d25659152
Yura <yury.kirytkin@yandex.ru>
2024-07-16 22:36:00 +0300

    Some refactoring

--- src/brain-calc-game.js
+++ src/brain-calc-game.js
@@ -13,30 +13,24 @@ export default (name) => {
   console.log("What is the result of the expression?");
   for (let i = 1; i <= 3; i += 1) {
     questionType = Math.floor(Math.random() * 3); // генерация случайного числа от 0 до 2
+    questionFirstNumber = Math.floor(Math.random() * maxNumber);
+    questionSecondNumber = Math.floor(Math.random() * maxNumber);
     switch (
       questionType // 3 возможных исхода 0(+), 1(-), 2(*)
     ) {
       case 1: {
-        questionFirstNumber = Math.floor(Math.random() * maxNumber);
-        questionSecondNumber = Math.floor(Math.random() * maxNumber);
         question = `${questionFirstNumber} - ${questionSecondNumber}`;
         expectedAnswer = questionFirstNumber - questionSecondNumber;
         break;
       }
       case 2: {
-        questionFirstNumber = Math.floor(
-          Math.random() * Math.floor(maxNumber / 10)
-        );
-        questionSecondNumber = Math.floor(
-          Math.random() * Math.floor(maxNumber / 10)
-        );
+        questionFirstNumber = Math.floor(questionFirstNumber / 10);
+        questionSecondNumber = Math.floor(questionFirstNumber / 10);
         question = `${questionFirstNumber} * ${questionSecondNumber}`;
         expectedAnswer = questionFirstNumber * questionSecondNumber;
         break;
       }
       default: {
-        questionFirstNumber = Math.floor(Math.random() * maxNumber);
-        questionSecondNumber = Math.floor(Math.random() * maxNumber);
         question = `${questionFirstNumber} + ${questionSecondNumber}`;
         expectedAnswer = questionFirstNumber + questionSecondNumber;
         break;
